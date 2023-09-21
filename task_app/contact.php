9f7fbefc32519559e68bcd785d496e914f0035f9
ElvisMwitari <mwitarielvis@gmail.com>
2023-09-21 10:01:11 +0300

    Initial commit

--- task_app/contact.php
+++ task_app/contact.php
@@ -2,8 +2,13 @@
     require_once "ClassAutoLoad.php";
 
         $OBJ_Layout->headers($conf);
+        
         $OBJ_Layout->logo($conf);
+
         $OBJ_Layout->navigation();
+
         $OBJ_Layout->banner();
+
         $OBJ_Forms->contact_us_form();
+
         $OBJ_Layout->footer($conf);
