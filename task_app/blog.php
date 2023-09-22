9013e9bfe6f2069339f8aeb58931d768f4217bfd
Jeremy Okuto <jerryokuto713@gmail.com>
2023-09-22 23:53:17 +0300

    Incomplete send mail function

--- task_app/blog.php
+++ task_app/blog.php
@@ -1,9 +1,9 @@
 <?php
     require_once "ClassAutoLoad.php";
 
-$details["sendToEmail"] = "benjamin.mundama@strathmore.edu";
-$details["sendToName"] = "BM";
-$details["emailSubjectLine"] = "OOP Test mail";
+$details["sendToEmail"] = "jeremyokuto231@gmail.com";
+$details["sendToName"] = "Jerry";
+$details["emailSubjectLine"] = "Test 1";
 $details["emailMessage"] = "Hi there this is another tests";
 
 $OBJ_SendMail->SendeMail($details, $conf);
--- task_app/config.php
+++ task_app/config.php
@@ -1,7 +1,7 @@
 <?php
 
 $conf["site_name"] = "ICS 2.2";
-$conf["au_email_address"] = "nick";
+$conf["au_email_address"] = "jeremy.okuto@strathmore.edu";
 
 $conf['db_type'] = "PDO";             /* Connection Type */
 $conf['db_hostname'] = "localhost";       /* Hostname */
@@ -9,3 +9,5 @@ $conf['db_port'] = "3306";             /* Port */
 $conf['db_username'] = "root";       /* Database Username */
 $conf['db_password'] = "";       /* Database User Password */
 $conf['db_name'] = "aden";             /* Database Name */
+$conf['api_key'] = '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';
+$conf['sender_name'] = "Jeremy Okuto";
