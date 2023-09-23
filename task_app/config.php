48b482cb1b4df3f17af91bbbf79d51855a94c6ba
NickoBeast1 <nickhesbourne58@gmail.com>
2023-09-23 11:41:56 +0300

    commit

--- task_app/config.php
+++ task_app/config.php
@@ -1,11 +1,18 @@
 <?php
 
-$conf["site_name"] = "ICS 2.2";
+<<<<<<< HEAD
+$conf["site_name"] = "Nick";
 $conf["au_email_address"] = "info@icsd.rochella.org";
+=======
+$conf["site_name"] = "ICS 2.2";
+$conf["au_email_address"] = "jeremy.okuto@strathmore.edu";
 
 $conf['db_type'] = "PDO";             /* Connection Type */
-$conf['db_hostname'] = "localhost";       /* Hostname */
-$conf['db_port'] = "3306";             /* Port */
+$conf['db_hostname'] = "127.0.0.1:3307";       /* Hostname */
+$conf['db_port'] = "3307";             /* Port */
 $conf['db_username'] = "root";       /* Database Username */
 $conf['db_password'] = "";       /* Database User Password */
 $conf['db_name'] = "aden";             /* Database Name */
+$conf['api_key'] = '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';
+$conf['sender_name'] = "Jeremy Okuto";
+$conf['username_email'] = 'apikey';
