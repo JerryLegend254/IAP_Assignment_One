3a75533651e78a5dc9e6aad21a5c31ef8415ac26
NickoBeast1 <nickhesbourne58@gmail.com>
2023-09-23 15:31:22 +0300

    Database changes

--- assignment_2/config/usersdatabase.sql
+++ assignment_2/config/usersdatabase.sql
@@ -10,21 +10,9 @@ CREATE TABLE IF NOT EXISTS `UsersTabled` (
 	`Username` varchar(62),
     `Email` varchar(100),
     `Passwords` varchar(58),
-    `is_admin` tinyint(1) NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`)
 );
-INSERT Users (Username, Full_Name, Passwords, Email, `is_admin`) values ('Cheese', 'Jerry', '$2y$10$Jr01rJA3zPPDC4ez0xzySO0U0MSn2toU3t/7wPaHhcCbI04c1/qlu', 'JerryMouse@cheese.com', 1); 
+INSERT Users (Username, Full_Name, Passwords, Email, `is_admin`) values ('NickoBeast', 'Nick Zola', '$2y$10$Jr01rJA3zPPDC4ez0xzySO0U0MSn2toU3t/7wPaHhcCbI04c1/qlu', 'nickzola@cheese.com', 1); 
 
 SELECT * FROM Users;
 
-DROP TABLE IF EXISTS `keys`;
-CREATE TABLE IF NOT EXISTS `keys` (
-    `id` int(5) NOT NULL AUTO_INCREMENT,
-    `key` varchar(100),
-    `user_id` int(5),
-    PRIMARY KEY (`id`)
-);
-
-INSERT into `keys` (`key`, `user_id`) values ('1234567890', 1);
-
-SELECT * FROM `keys`;
