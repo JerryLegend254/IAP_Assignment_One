a354ac279cbb7fa531ab22e091a2bcc5f33ce44d
NickoBeast1 <nickhesbourne58@gmail.com>
2023-09-23 15:38:37 +0300

    Minor Improvemts

--- task_app/config.php
+++ task_app/config.php
@@ -6,8 +6,8 @@ $conf["site_name"] = "NickoBeast";
 $conf["au_email_address"] = "jeremy.okuto@strathmore.edu";
 
 $conf['db_type'] = "PDO";             /* Connection Type */
-$conf['db_hostname'] = "127.0.0.1:3307";       /* Hostname */
-$conf['db_port'] = "3307";             /* Port */
+$conf['db_hostname'] = "localhost";       /* Hostname */
+$conf['db_port'] = "3306";             /* Port */
 $conf['db_username'] = "root";       /* Database Username */
 $conf['db_password'] = "";       /* Database User Password */
 $conf['db_name'] = "aden";             /* Database Name */
